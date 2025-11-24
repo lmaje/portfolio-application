f0fae0484ff83bb5288d36a449863f05d93e2657
ShahirWardak <shahir.wardak@rspb.org.uk>
2025-11-24 21:08:13 +0000

    Added heading

---
+++ app/components/block.tsx
@@ -0,0 +1,11 @@
+import "../styles/page.css";
+export default function Block() {
+  return (
+    <>
+      <div className="div0">
+        <h1 className="h1">Laila Majeed</h1>
+      </div>
+      <div className="div1">DIV BLOCK Banana</div>
+    </>
+  );
+}
