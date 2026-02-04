b4415d6abd87b8289b31daa16011184e30d46583
Laila Majeed <lailamajeed@Lailas-MacBook-Pro.local>
2026-02-04 21:07:18 +0000

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Block from "./components/block";
+import "./styles/page.css";
+export default function Home() {
+  return (
+    <div className="background">
+      <Block></Block>
+    </div>
+  );
+}
